7e18586f170523e365529d49f394d14591996ce4
abahwisdom <abahwisdom@gmail.com>
2024-07-01 23:58:17 +0100

    test: add e2e tests for url

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,10 +3,10 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('UrlController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -15,10 +15,65 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/encode (POST)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .post('/encode')
+      .send({ longUrl: 'https://example.com' })
+      .expect(201)
+      .expect((res) => {
+        expect(res.body).toHaveProperty('shortUrl');
+      });
+  });
+
+  it('/decode (POST)', async () => {
+    const encodeResponse = await request(app.getHttpServer())
+      .post('/encode')
+      .send({ longUrl: 'https://example.com' })
+      .expect(201);
+
+    const shortUrl = encodeResponse.body.shortUrl.split('/').pop();
+
+    return request(app.getHttpServer())
+      .post('/decode')
+      .send({ shortUrl })
+      .expect(201)
+      .expect((res) => {
+        expect(res.body).toHaveProperty('longUrl', 'https://example.com');
+      });
+  });
+
+  it('/statistic/:urlPath (GET)', async () => {
+    const encodeResponse = await request(app.getHttpServer())
+      .post('/encode')
+      .send({ longUrl: 'https://example.com' })
+      .expect(201);
+
+    const urlPath = encodeResponse.body.shortUrl.split('/').pop();
+
+    return request(app.getHttpServer())
+      .get(`/statistic/${urlPath}`)
       .expect(200)
-      .expect('Hello World!');
+      .expect((res) => {
+        expect(res.body).toHaveProperty('longUrl', 'https://example.com');
+        expect(res.body).toHaveProperty('visits', 0);
+      });
+  });
+
+  it('/:urlPath (GET)', async () => {
+    const encodeResponse = await request(app.getHttpServer())
+      .post('/encode')
+      .send({ longUrl: 'https://example.com' })
+      .expect(201);
+
+    const urlPath = encodeResponse.body.shortUrl.split('/').pop();
+
+    return request(app.getHttpServer())
+      .get(`/${urlPath}`)
+      .expect(302)
+      .expect('Location', 'https://example.com');
   });
 });

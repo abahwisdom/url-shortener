adf2db221db8433cf9524cc0b75f99553a2eb54a
abahwisdom <abahwisdom@gmail.com>
2024-07-01 12:59:49 +0100

    feat: add url controller

--- src/url/url.controller.ts
+++ src/url/url.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Res } from '@nestjs/common';
+import { UrlService } from './url.service';
 
-@Controller('url')
-export class UrlController {}
+@Controller()
+export class UrlController {
+  constructor(private readonly urlService: UrlService) {}
+
+  @Post('encode')
+  async encode(@Body('longUrl') longUrl: string) {
+    const shortUrl = await this.urlService.encode(longUrl);
+    return { shortUrl };
+  }
+
+  @Post('decode')
+  async decode(@Body('shortUrl') shortUrl: string) {
+    const longUrl = await this.urlService.decode(shortUrl);
+    return { longUrl };
+  }
+
+  @Get('statistic/:urlPath')
+  async getStatistics(@Param('urlPath') urlPath: string) {
+    const stats = await this.urlService.getStatistics(urlPath);
+    return stats;
+  }
+
+ @Get(':urlPath')
+  async redirect(@Param('urlPath') urlPath: string, @Res() res) {
+    const longUrl = await this.urlService.decode(urlPath);
+    await this.urlService.incrementVisits(urlPath);
+
+    // Ensure the longUrl includes the protocol
+    const isFullUrl = /^https?:\/\//i.test(longUrl);
+    const redirectUrl = isFullUrl ? longUrl : `http://${longUrl}`;
+
+    return res.redirect(redirectUrl);
+  }
+}
